a91c2e5722d98ddaa97855ba121af222190213fc
Derek Scruggs <djscruggs@digitalbazaar.com>
2026-09-04 16:23:10 -0500

    Address repository setup review comments.
    Update the CI workflow to `actions/checkout@v7`, `actions/setup-node@v7`,
    and Node 26.x, and name the setup step for the version rather than
    repeating it.
    
    Stop tracking `package-lock.json`, which is not usually committed for a
    package of this type, and ignore it along with other common artifacts.
    
    Switch to the `vue3-recommended` eslint preset, which fits a package
    shipping `.vue` components, and update `eslint` and
    `@digitalbazaar/eslint-config` to their current major versions.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 /*!
  * Copyright 2026 Digital Bazaar, Inc.
  */
-import config from '@digitalbazaar/eslint-config/node-recommended';
+import config from '@digitalbazaar/eslint-config/vue3-recommended';
 
 export default [...config];
